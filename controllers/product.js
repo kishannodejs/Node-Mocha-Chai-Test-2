9c86b81f010a2515d7efeb2d3c1857b0b905e30a
Kishan Prajapati <kishan.nodejs@gmail.com>
2022-07-03 22:47:16 +0530

    Node testing

--- controllers/product.js
+++ controllers/product.js
@@ -2,6 +2,7 @@ var Product = require('../models/product');
 
 //Simple version, without validation or sanitation
 exports.test = function (req, res) {
+    console.log("AAAAAAAAAAAAA");
     res.send('Greetings from the Test controller!');
 };
 
@@ -25,19 +26,35 @@ exports.product_create = async function (req, res) {
     })
 };
 
-exports.product_getall = function (req, res) {
-    Product.find(req.params.id, function (err, product) {
-        if (err) return next(err);
-        res.send(product);
-    })
-};
 
-exports.product_details = function (req, res) {
-    Product.findById(req.params.id, function (err, product) {
-        if (err) return next(err);
-        res.send(product);
+
+exports.product_getall = async (req, res) => {
+    console.log("This is get all to do from Controller");
+    await Product.find().then((products) => {
+      res.send({products});
+    }).catch((err) => {
+      res.status(400).send(err);
     })
-};
+  };
+
+// exports.product_details = function (req, res) {
+
+//     console.log(req.params);
+//     Product.findById(req.params.id, function (err, product) {
+//         if (err) return next(err);
+//         res.send(product);
+//     })
+// };
+
+exports.product_details = async (req, res) => {
+    console.log(req.params);
+    console.log("This is get all to do from Controller single product");
+    await Product.findById(req.params.id).then((products) => {
+      res.send({products});
+    }).catch((err) => {
+      res.status(400).send(err);
+    })
+  };
 
 exports.product_update = function (req, res) {
     Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
--- mytest.js
+++ mytest.js
@@ -1,6 +1,7 @@
 const expect = require('expect');
 const request = require('supertest');
-const {ObjectID} = require('mongodb');
+//const {ObjectID} = require('mongodb');
+let mongoose = require('mongoose');
 let chai = require("chai");
 let chaiHttp = require("chai-http");
 var should = chai.should();
@@ -32,6 +33,26 @@ const {Product} = require('./models/product');
 // });
 
 
+
+
+
+describe('Get /products/getall', () => {
+
+  //let query = Product.find({});
+
+//console.log(query);
+  it('should get all products', (done) => {
+    request(app)
+      .get('/products/getall')
+      //.set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        console.log(res.body.products);
+     // expect(res.body.products.length).toBe(15)
+      })
+      .end(done);
+  })
+});
 
 describe('POST /products/create', () => {
    it('should create a new product', (done) => {
@@ -73,3 +94,44 @@ describe('POST /products/create', () => {
    //     })
    // });
 });
+
+
+describe('GET /products/:id', () => {
+  //console.log(`PPPPPPPPP/products/${products[0]._id.toHexString()}PPPPPPPPPPPP`);
+  it('should return products doc by id', (done) => {
+    request(app)
+      .get(`/products/62c16f9219ec7e29d121378f`)
+     // .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        //expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+});
+
+
+describe('/GET/:id products', () => {
+  it('it should GET a book by the given id', (done) => {
+      let product = new Product({ name: "The Lord of the Rings", price: 1170 });
+      product.save((err, product) => {
+          chai.request(server)
+        .get('/products/' + product.id)
+        .send(product)
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              res.body.should.have.property('title');
+              res.body.should.have.property('author');
+              res.body.should.have.property('pages');
+              res.body.should.have.property('year');
+              res.body.should.have.property('_id').eql(product.id);
+          done();
+        });
+      });
+
+  });
+});
+
+///https://www.digitalocean.com/community/tutorials/test-a-node-restful-api-with-mocha-and-chai
